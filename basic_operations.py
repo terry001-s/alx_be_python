e13250da9ddf2e0dc626b3226dcb037a89fe6a7d
Terry Yeboah <terryyeboah71@gmail.com>
2025-09-12 18:19:25 -0800

    Commited the basic_operations files

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+Addition = number1 + number2
+Subtraction = number2 - number1
+Multiplication = number1 * number2
+print("Addition of ",number1, "and" ,number2, "is", Addition)
+print("Subtraction of ",number1, "and", number2, "is", Subtraction)
+print("Multiplication of ",number1, "and" ,number2, "is", Multiplication )
+
