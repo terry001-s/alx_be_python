fa012c4ff72b5dec781ee4d779343ea884c80bdc
Terry Yeboah <terryyeboah71@gmail.com>
2025-09-20 15:51:59 -0800

    If/elif/else on a weather input

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather = ["sunny","rainy","cold"]
+Question = input(f"What's the weather like today? (sunny/rainy/cold): ").lower()
+if Question == "sunny":
+    print(" Wear a t-shirt and sunglasses.")
+elif Question == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif Question == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else :
+    print("Sorry, I don't have recommendations for this weather.")
