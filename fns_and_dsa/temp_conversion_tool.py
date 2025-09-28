a4944926bb5069f34a4c46eff437ead55aee209a
Terry Yeboah <terryyeboah71@gmail.com>
2025-09-28 00:30:34 -0800

    defined the global scope well

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,9 @@
 # temp_conversion_tool.py
 
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+
 
 
 def convert_to_celsius(fahrenheit):
